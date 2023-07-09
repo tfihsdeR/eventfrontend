66cea9c4baad612f17ddae16f859e60357536c5c
tfihsdeR <erdincatay@yandex.com>
2023-07-09 11:19:11 +0300

    Update_AboutUs

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
 function AboutUs() {
+    const pStyles = {
+        fontFamily: "Acakadut",
+        fontSize: "50px"
+    };
+
+    const h1Styles = {
+        fontFamily: "Silverstone",
+        fontSize: "100px"
+    };
+
     return (
-        <div>AboutUs</div>
+        <div>
+            <h1 style={h1Styles}>Erdinç Atay</h1>
+            <p style={pStyles}>E-Mail: erdincatay@yandex.com</p>
+            <p style={{ ...pStyles }}><a style={{ textDecoration: "none", color: "black" }} href="https://github.com/tfihsdeR">GitHub: https://github.com/tfihsdeR</a></p>
+            <p style={pStyles}>Please follow me :)</p>
+        </div>
     )
 }
 
