8da637130772caca6419ac75beb22d443ff09076
tfihsdeR <erdincatay@yandex.com>
2023-07-10 13:01:34 +0300

    Update_Icons

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import '@fortawesome/fontawesome-free/css/all.css';
 
 function AboutUs() {
+
+    const recipientEmail = 'erdincatay@yandex.com';
+    const subject = 'About Event Full Stack Project';
+    const body = 'I hope this email makes you well.';
+
+    const mailtoLink = `mailto:${recipientEmail}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
     const pStyles = {
         fontFamily: "Acakadut",
         fontSize: "50px"
@@ -11,12 +19,41 @@ function AboutUs() {
         fontSize: "100px"
     };
 
+    const instagramIconStyes = {
+        fontSize: "60px",
+        color: "red",
+        marginRight: "15px"
+    };
+
+    const linkedinIconStyes = {
+        fontSize: "60px",
+        color: "blue",
+        marginRight: "15px"
+    };
+
+    const mailIconStyles = {
+        fontSize: "60px",
+        color: "orange"
+    };
+
     return (
         <div>
             <h1 style={h1Styles}>Erdinç Atay</h1>
             <p style={pStyles}>E-Mail: erdincatay@yandex.com</p>
             <p style={{ ...pStyles }}><a style={{ textDecoration: "none", color: "black" }} href="https://github.com/tfihsdeR">GitHub: https://github.com/tfihsdeR</a></p>
             <p style={pStyles}>Please follow me :)</p>
+
+            <a href="https://www.instagram.com/_erdinc.atay_/" target="_blank" rel="noopener noreferrer" style={instagramIconStyes}>
+                <i className="fab fa-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/erdincatay" target="_blank" rel="noopener noreferrer" style={linkedinIconStyes}>
+                <i className="fab fa-linkedin"></i>
+            </a>
+
+            <a href={mailtoLink} style={mailIconStyles}>
+                <i className="far fa-envelope"></i>
+            </a>
         </div>
     )
 }
--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -6,8 +6,20 @@ function SearchResults() {
     const { keyword } = useParams();
     const navigate = useNavigate();
 
+    const [ascendingBtnColor, setascendingBtnColor] = useState("white");
+    const [descendingBtnColor, setdescendingBtnColor] = useState("white");
+
     const [searchedEvents, setSearchedEvents] = useState(null);
     const [allImages, setAllImages] = useState(null);
+    const [searchedVenues, setSearchedVenues] = useState(null);
+
+    const [firstDate, setfirstDate] = useState(null);
+    const [secondDate, setsecondDate] = useState(null);
+    const [selectedVenue, setselectedVenue] = useState();
+    const [detailedSearchKeyword, setdetailedSearchKeyword] = useState(null);
+    const [isAscending, setisAscending] = useState(true);
+
+
 
     useEffect(() => {
         fetch(`https://localhost:7191/api/Event/events/keyword?keyword=${keyword}`)
@@ -22,7 +34,7 @@ function SearchResults() {
             })
             .catch(error => {
                 console.error(error);
-                setSearchedEvents([]);
+                // setSearchedEvents([]);
             });
 
         fetch(`https://localhost:7191/api/Image/Image/${keyword}`)
@@ -37,9 +49,132 @@ function SearchResults() {
             })
             .catch(error => {
                 console.error(error);
-                setAllImages([]);
+                // setAllImages([]);
             });
+
+        fetch("https://localhost:7191/api/Venue")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                }
+            })
+            .then(data => {
+                let sortedVenues = data.sort((a, b) => {
+                    const nameA = a.name.toLowerCase();
+                    const nameB = b.name.toLowerCase();
+
+                    if (nameA > nameB) {
+                        return -1;
+                    } else if (nameA < nameB) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+
+                let reverseVenues = sortedVenues.reverse();
+
+                setSearchedVenues(reverseVenues);
+            })
+            .catch(error => {
+                console.error(error);
+                // setSearchedVenues([]);
+            })
     }, [keyword]);
+
+    //#region FUNCTIONS
+    const Search = async () => {
+        await fetch(`https://localhost:7191/api/Event/events/keyword?keyword=${searchedEvents}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                }
+            })
+            .then(data => {
+                setSearchedEvents(data);
+            })
+            .catch(error => {
+                console.error(error);
+                // setSearchedEvents([]);
+            });
+
+        await fetch(`https://localhost:7191/api/Image/Image/${searchedEvents}`)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                }
+            })
+            .then(data => {
+                setAllImages(data);
+            })
+            .catch(error => {
+                console.error(error);
+                // setAllImages([]);
+            });
+
+        const filteredEvents = searchedEvents.filter(event => {
+            const endDate = new Date(event.endDate);
+            const startDateRangeStart = new Date(firstDate);
+            const endDateRangeEnd = new Date(secondDate);
+
+            return endDate <= endDateRangeEnd && endDate >= startDateRangeStart
+        });
+
+        const sortedEvents = filteredEvents.sort((a, b) => {
+            const nameA = a.name.toLowerCase();
+            const nameB = b.name.toLowerCase();
+
+            if (nameA > nameB) {
+                return -1;
+            } else if (nameA < nameB) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        let reverseVenues = isAscending ? sortedEvents.reverse() : sortedEvents;
+
+        setSearchedEvents(reverseVenues);
+
+        console.log("SearchedEvents: ", searchedEvents);
+
+        navigate(`/searchResults/${detailedSearchKeyword}`);
+    }
+
+    const AscendingClick = () => {
+        setascendingBtnColor("red");
+        setdescendingBtnColor("white");
+        setisAscending(true);
+    }
+
+    const DescendingClick = () => {
+        setdescendingBtnColor("red");
+        setascendingBtnColor("white");
+        setisAscending(false);
+    }
+
+    const handleFirstDateChange = (event) => {
+        const selectedDate = event.target.value;
+        const [month, day, year] = selectedDate.split('/');
+
+        const formattedDate = `${day}/${month}/${year}`;
+
+        setfirstDate(formattedDate);
+    };
+
+    const handleSecondDateChange = (event) => {
+        const selectedDate = event.target.value;
+        const [month, day, year] = selectedDate.split('/');
+
+        const formattedDate = `${day}/${month}/${year}`;
+
+        setsecondDate(formattedDate);
+    };
+    //#endregion
 
     //#region STYLES
     const errorTextStyles = {
@@ -90,26 +225,110 @@ function SearchResults() {
         justifyContent: "space-around",
         flexWrap: "wrap",
     };
+
+    const detailedSearchContainerStyles = {
+        display: "inline-flex",
+        width: "100%",
+        justifyContent: "center"
+    };
+
+    const dateContainerStyles = {
+        display: "inline-flex",
+        flexWrap: "wrap"
+    };
+
+    const inputDateStyle = {
+        borderRadius: "5px"
+    };
+
+    const selectingStyles = {
+        display: "inline-flex",
+        justifyContent: "space-around",
+        width: "70%",
+        alignItems: "center",
+        flexWrap: "wrap"
+    };
+
+    const searchButtonStyles = {
+        width: "100px",
+        height: "30px",
+        borderRadius: "10px"
+    };
+
+    const dropdownStyles = {
+        borderRadius: "10px",
+        width: "250px"
+    };
     //#endregion
 
     return (
         <div>
-            {
-                searchedEvents != null && allImages != null ? (
-                    <div style={searchedItemsContainer}>
-                        {searchedEvents.map((e, index) => (
-                            <div key={index} style={imageContainerStyles}>
-                                <div style={linkStyles} onClick={() => navigate(`/eventDetails/${e.id}`)}>
-                                    <div style={theatreImageStyles(allImages, e.id)}></div>
-                                    <div style={imageTextStyles}><span>{e.name}</span></div>
-                                </div>
+            <div>
+                <h2>Detailed Search</h2>
+                <div style={detailedSearchContainerStyles}>
+                    <div style={selectingStyles}>
+                        <div style={dateContainerStyles}>
+                            <div style={{ marginRight: "10px" }}>
+                                <label for="firstDate">From: </label>
+                                <input type="date" id='firstDate' name='firstDate' style={inputDateStyle} onChange={handleFirstDateChange} />
                             </div>
-                        ))}
+
+                            <div>
+                                <label for="secondDate">To: </label>
+                                <input type="date" id='secondDate' name='secondDate' style={inputDateStyle} onChange={handleSecondDateChange} />
+                            </div>
+
+
+                        </div>
+
+                        <div >
+                            <button style={{ borderRadius: "10px", backgroundColor: ascendingBtnColor }} onClick={AscendingClick}>Ascending</button>
+                            <button style={{ borderRadius: "10px", marginLeft: "10px", backgroundColor: descendingBtnColor }} onClick={DescendingClick}>Descending</button>
+                        </div>
+
+                        <div>
+                            <label for="venues" style={{ marginRight: "10px" }}>Venue:</label>
+                            <select id="venues" name="venues" style={dropdownStyles} onChange={v => setselectedVenue(v.target.value)}>
+                                <option value="default">Select Venue</option>
+                                {
+                                    searchedVenues != null ? (
+                                        searchedVenues.map((v, index) => {
+                                            return (
+                                                <option key={index}>{v.name}</option>
+                                            )
+                                        })
+                                    ) : null
+                                }
+                            </select>
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder='Search' style={{ borderRadius: "10px" }} onChange={k => setdetailedSearchKeyword(k.target.value)} />
+                        </div>
                     </div>
-                ) : (
-                    <span style={errorTextStyles}>There is no event to display!</span>
-                )
-            }
+                </div>
+                <div style={{ marginTop: "30px" }}>
+                    <button style={searchButtonStyles} onClick={Search}>Search</button>
+                </div>
+            </div>
+            <div style={{ marginTop: "30px" }}>
+                {
+                    searchedEvents != null && allImages != null ? (
+                        <div style={searchedItemsContainer}>
+                            {searchedEvents.map((e, index) => (
+                                <div key={index} style={imageContainerStyles}>
+                                    <div style={linkStyles} onClick={() => navigate(`/eventDetails/${e.id}`)}>
+                                        <div style={theatreImageStyles(allImages, e.id)}></div>
+                                        <div style={imageTextStyles}><span>{e.name}</span></div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    ) : (
+                        <span style={errorTextStyles}>There is no event to display!</span>
+                    )
+                }
+            </div>
         </div>
     );
 
